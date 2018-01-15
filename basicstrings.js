f6f45fa3c45112e057dc2a8f3aaac0da39dc55a9
Sabrina Markon <phpsitescripts@outlook.com>
2018-01-15 13:06:46 -0700

    Initial Commit

---
+++ basicstrings.js
@@ -0,0 +1,13 @@
+let str = "I like cats";
+
+// Find the length of a string. PHP compare strlen()
+
+let stringlength = str.length;
+console.log(str + ' is ' + stringlength + ' characters.');
+
+// Find character at string index. PHP compare str_split() into an array, str[], then str[0]
+
+let firstchar = str[0];
+console.log('The first character of ' + str . ' is ' . firstchar);
+
+// 
