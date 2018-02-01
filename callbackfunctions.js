414b06c14d43eca4595f8be430bf9b243a5d7d7e
Sabrina Markon <phpsitescripts@outlook.com>
2018-01-31 18:28:15 -0700

    Examples of how callback functions can be used with a JavaScript object/JSON, and using the filter command to select specific data from the object.

--- callbackfunctions.js
+++ callbackfunctions.js
@@ -39,3 +39,62 @@ theButt.addEventListener('click', (e) => {
     setTimeout(LogCall, 3000); // happens 3 secotechs after clicking the button. So may end up anywhere between the function calls above.
 });
 
+/* Example of more fun with callback functions */
+
+// 1: let's make an object:
+let techStack = [
+    {name: 'PHP', years: 20, level: 'Advanced'},
+    {name: 'JavaScript', years: 20, level: 'Intermediate'},
+    {name: 'React', years: 1, level: 'Novice'},
+    {name: 'Redux', years: 1, level: 'Novice'},
+    {name: 'Git', years: 3, level: 'Intermediate'},
+    {name: 'HTML', years: 25, level: 'Advanced'},
+    {name: 'Python', years: 2, level: 'Novice'},
+    {name: 'R', years: 1, level: 'Novice'},
+    {name: 'CSS', years: 20, level: 'Advanced'},
+    {name: 'Java', years: 2, level: 'Novice'},
+    {name: 'Laravel', years: 3, level: 'Intermediate'},
+    {name: 'TypeScript', years: 1, level: 'Novice'},
+    {name: 'Lua', years: 1, level: 'Novice'},
+    {name: 'Linux', years: 20, level: 'Intermediate'}
+];
+
+// 2: then we will make a function that uses a callback:
+let processTechStack = (data, callback) => {
+    // filter each item in the object to find the ones only that we want:
+    let filteredTechStack = data.filter(tech => tech.level === 'Advanced');
+    for (let tech of filteredTechStack) {
+        if (typeof callback === 'function') {
+            callback(tech);
+        } else {
+            console.log('The callback was not even a function!');
+        }
+    }
+}
+
+// 3: here is the callback function for #2:
+processTechStack(techStack, function(techStackobject) {
+    if (techStackobject.years <= 25) {
+        console.log(techStackobject.name);
+    }
+});
+
+// 4: Another way to do the same thing, using a more 'traditional' for loop (and no fat arrows..):
+let processTechStack2 = function(data, callback) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].level.toLowerCase() === 'intermediate') {
+            if (typeof callback === 'function') {
+                callback(data[i]);
+            } else {
+                console.log('The callback was not a funciton!');
+            }
+        }
+    }
+}
+
+// 5: here is the callback function for #4, which is identical to the first (in #3):
+processTechStack2(techStack, function(techStackobject) {
+    if (techStackobject.years > 2) {
+        console.log(techStackobject.name);
+    }
+});
