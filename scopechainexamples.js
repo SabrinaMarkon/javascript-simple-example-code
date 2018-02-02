6dc3be40c7958feece7a1a065b2cd87f6abd1672
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-01 23:15:28 -0700

    Notes and examples of var vs let for declaring JavaScript variables

--- scopechainexamples.js
+++ scopechainexamples.js
@@ -10,22 +10,31 @@ set of rules where in a program we can access referenced items, such as variable
 When variable lookups occur, it first checks for the variable value in its own scope. If not found, it looks for it in the next scope up (in a chain until global at the top.)
 
 For instance:
-Below, in the console.log(num1 + b) line, we find both num1 and b in the #2 scope. 
-But for the console.log(num2 + a) line, while num2 is found in the #3 scope, the variable a is not found in #3 or then #2 either. It is finally found in the global scope.
+Below, in the console.log(num1 + b) line, we find both num1 and b in the line's own scope, #2.
+But for the console.log(num2 + a) line, while num2 is found in the #3 scope, the variable a is not found in #3 or then #2 either. It is finally found in the global scope.a
+
+NOTE: in this example code, 'let' ends up working the same as 'let'.
 
 */
 
 let a = 10; // #1 scope - global
+
+let d = 2; // #1 scope - global
 
 //===================================================
 
 let add5 = function(num1) { // add5 is global, the function(num1) in it however is #2 scope.
 
     let b = 5;  // also #2 scope.
+    let d = 1; // also #2 scope (observe d is also in the global scope with a different value!!) 
     console.log(num1 + b); 
+
+    console.log(num1 + d); // will use the let d = 1 in the current scope, rather than the global!!!
 
     let add10 = function(num2) {
         console.log(num2 + a); // add10 is #2 scope, but the function(num2) within it is #3 scope.
+
+        //*** variable 'a' SCOPE CHAIN! a is not in #3 scope, so it checks the next scope up, #2, and it isn' found there either, so it checks #1 (the global scope) and finds the value there to use here.  If 'a' was not found, an error would occur because of an undefined variable. ***/
     }
 
     add10(3); // #2 scope
@@ -34,6 +43,19 @@ let add5 = function(num1) { // add5 is global, the function(num1) in it however 
         let c = 15; // also #4 scope.
         console.log(num3 + c);
     }
+
+    let add15 = function(num4) { // add15 is in #2 scope, but the function(num4) within it is #5 scope. 
+        
+        //console.log(num4 + c); 
+
+        /*** This will cause a REFERENCE ERROR ('c is not defined') because variable 'c' is NOT in this function's SCOPE CHAIN (which goes #5 -> #2 -> #1 (global)!!! Notice that the let c = 15 is in #4 scope which is a sibling and NOT part of the scope chain for the add15 function!
+        ***/
+
+        add10(4); // we can't find this function in the current scope, scope #5, so we go up a level and can indeed find it in scope #2 which is part of the scope chain for add15, so this will work!
+
+    }
+
+    add15(5); // will produce an error because 'c' is not in the scope chain.
 
     add20(4); // #2 scope.
 
---
+++ varvsletexamples.js
@@ -0,0 +1,54 @@
+/*
+"var vs let" EXAMPLE 1:
+------------------------------
+
+- scope of variables declared with var is the function or the global environment. Variables declared with var ARE HOISTED.
+
+- scope of variables declared with let is the code block {}. Variables declared with let are NOT HOISTED.
+(All Things Javascript, LLC, 2016)
+*/
+
+var funct1 = function() {
+    console.log("1: " + x);
+};
+
+funct1();
+
+// x is declared AFTER funct1() is called that needs x.
+
+/*
+With 'var':
+the console says that x is simply 'undefined': x exists because it is referenced in funct1, but it is not assigned a value yet because var x = 10 occurs AFTER the call to funct1.  It does NOT give an error as it will with 'let', which means that with 'var' it was HOISTED to the TOP OF THE CODE.
+*/
+var x = 10;  
+
+/*
+With 'let':
+also in the global environment the same as var x = 10 was, but is NOT HOISTED, so funct1 doesn't even know x  exists at all and the script stalls in an uncaught reference error.
+*/
+// let x = 10; 
+
+//======================================================
+
+/*
+"var vs let" EXAMPLE 2:
+-------------------------------
+*/
+
+// with 'var i = 1': it will show 1, 2, 3, 4, 5 (where 5 is the console.log OUTSIDE the loop..the final value of i).
+var funct2 = function() {
+    for (var i = 1; i < 5; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
+funct2();
+
+// with 'let i = 1': it will show 1,2,3,4,then Uncaught Reference error, because i is not defined outside the CODE BLOCK (which was the for loop!)
+var funct3 = function() {
+    for (let i = 1; i < 5; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
+funct3();
