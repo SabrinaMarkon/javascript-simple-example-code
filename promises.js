9b68ad6089b750a083dc4191a96df5b4ec9278b0
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-09 23:56:49 -0700

    Added Promise chain

--- promises.js
+++ promises.js
@@ -3,12 +3,13 @@
 /*
 resolve() - promise is fulfilled.
 then() - method gets executed when promise is resolved.
-fromResolve - parameter to callback function in then() method that accepts output from resolve('whatever is here')
+fromResolve - parameter to callback function in then() method that accepts output from resolve('whatever is here') - can be other name not just fromResolve.
 reject() - promise is not fulfilled.
 catch() - method that is executed when promise fails.
-fromReject - parameter to callback function in catch() method that accepts output from reject.
+fromReject - parameter to callback function in catch() method that accepts output from reject - can be other name not just fromReject. 
 */
 
+/* =================== SIMPLE EXAMPLE ====================*/
 let iSwearToCleanTheCatLitter = new Promise(function(resolve, reject) {
 
     // TESTS:
@@ -36,3 +37,50 @@ iSwearToCleanTheCatLitter.then(function(fromResolve) {
 
 });
 
+
+/* =================== FANCY EXAMPLE ====================*/
+
+/* Here, you have to finish something, before you start another thing */
+
+// 1) do first. Read the textbook lesson.
+let readTextbook = function() {
+    return new Promise(function(resolve, reject) {
+        resolve('Read the textbook chapter');
+    });
+}
+
+// 2) after I read, I want to write my assignment.
+let writeAssignment = function(prom) {
+    return new Promise(function(resolve, reject) {
+        resolve('Wrote the assignment');
+    });
+}
+
+// 3) after I am done my assignment, I need to grade my peers.
+let gradePeers = function(prom) {
+    return new Promise(function(resolve, reject) {
+        resolve('Graded peer papers');
+    });
+}
+
+// 4) after I am done grading my peers, I write the exam.
+let writeExam = function(prom) {
+    return new Promise(function(resolve, reject) {
+        resolve('Got 100% on the text!');
+    });
+}
+
+/*
+SO let's write a nice nested promise:
+First, execute the first function and it returns its promise to the then() function.
+
+*/
+readTextbook().then(function() {
+    return writeAssignment();
+}).then(function() {
+    return gradePeers();
+}).then(function() {
+    return writeExam();
+}).then(function() {
+    console.log('All done!');
+});
