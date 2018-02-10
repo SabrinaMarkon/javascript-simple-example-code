fd83432da3c2a0872e6d180862378babd766c5be
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-10 00:19:21 -0700

    Example of Promises if we want them all to spawn at the same time instead of being chained one after the other

--- promises.js
+++ promises.js
@@ -38,9 +38,12 @@ iSwearToCleanTheCatLitter.then(function(fromResolve) {
 });
 
 
-/* =================== FANCY EXAMPLE ====================*/
+/* =================== FANCY EXAMPLE 1: Chained Promises ====================*/
 
-/* Here, you have to finish something, before you start another thing */
+/*
+Here, you have to finish something, before you start another thing 
+Promises are much cleaner code than a nested chain of callback function pyramid of nightmares.
+*/
 
 // 1) do first. Read the textbook lesson.
 let readTextbook = function() {
@@ -50,37 +53,50 @@ let readTextbook = function() {
 }
 
 // 2) after I read, I want to write my assignment.
-let writeAssignment = function(prom) {
+let writeAssignment = function(message) {
     return new Promise(function(resolve, reject) {
-        resolve('Wrote the assignment');
+        resolve(message + ', wrote the assignment');
     });
 }
 
 // 3) after I am done my assignment, I need to grade my peers.
-let gradePeers = function(prom) {
+let gradePeers = function(message) {
     return new Promise(function(resolve, reject) {
-        resolve('Graded peer papers');
+        resolve(message + ', graded peer papers');
     });
 }
 
 // 4) after I am done grading my peers, I write the exam.
-let writeExam = function(prom) {
+let writeExam = function(message) {
     return new Promise(function(resolve, reject) {
-        resolve('Got 100% on the text!');
+        resolve(message + ', and got 100% on the unit test!');
     });
 }
 
 /*
-SO let's write a nice nested promise:
+SO let's write a nice tidy nested promise:
 First, execute the first function and it returns its promise to the then() function.
 
+For every resolve() we are getting a message back into the next then() function. We can catch that message in the 'result' parameters.
 */
-readTextbook().then(function() {
-    return writeAssignment();
-}).then(function() {
-    return gradePeers();
-}).then(function() {
-    return writeExam();
-}).then(function() {
-    console.log('All done!');
+readTextbook().then(function(result) {
+    return writeAssignment(result);
+}).then(function(result) {
+    return gradePeers(result);
+}).then(function(result) {
+    return writeExam(result);
+}).then(function(result) {
+    //console.log('All done!');
+    console.log(result);
+});
+
+/* =================== FANCY EXAMPLE 2: All At Once ====================*/
+/*
+What if, instead of the above where one promise runs after the other, we want them all to spawn
+at the same time, so that once ALL of them are done, only then will we do something.
+Notice that although the above chained set of Promises come before the below Promise.all(),
+the Promise.all() finishes execution first (faster, because all run at once!)
+*/
+Promise.all([readTextbook(), writeAssignment(), gradePeers(), writeExam()]).then(function() {
+    console.log('All finished at once!');
 });
