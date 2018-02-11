1e822097b80c3dd259040f8b06ff96360172b821
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-11 01:24:45 -0700

    Fat arrow functions, arguments variable, and the spread operator

--- fatarrowfunctions.js
+++ fatarrowfunctions.js
@@ -1,6 +1,6 @@
 /*  Fat Arrow Functions */
 
-// 1) Lexical 'this'
+// 1) Lexical 'this'. Fat arrow functions do not have their own 'this':
 var cat = function() {
     this.val = 1;
     setTimeout(function() {
@@ -12,7 +12,7 @@ var cat = function() {
 };
 var catcat = new cat();
 
-// Fat arrow functions do not have their own 'this', so using one fixes the problem because this.val will refer to the parent's 'this' instead:
+// Fat arrow functions do not have their own 'this', so using one fixes the problem because this.val will refer to the parent's 'this' instead. 
 var dog = function() {
     this.val = 1;
     setTimeout(() => {
@@ -22,4 +22,27 @@ var dog = function() {
 }
 var dogdog =  new dog();
 
-// 2) 
+/*====================================================*/
+
+// 2) Fat arrow functions do NOT have the 'arguments' variable.
+
+// normal:
+var corvids = function() {
+    /* the below line will work and for this example show 'crow', because it is first in the arguments array. Even though the function() has no parameters defined between the parenthesis! */
+    console.log(arguments[0]); // prints 'crow'
+    console.log(arguments[2]); //prints 'bluejay'
+};
+corvids('crow', 'magpie', 'bluejay', 'raven');
+
+// fat arrow:
+var songbirds = () => {
+    console.log(arguments[0]); // Error! "Uncaught ReferenceError: arguments is not defined" because again, fat arrow functions DO NOT HAVE an 'arguments' parameter!
+};
+//songbirds('warbler', 'cardinal', 'painted bunting', 'canary');
+
+// We can fix it though with the SPREAD OPERATOR (ECMA6)
+var  raptors = (...args) => {
+    console.log(args[0]); // prints 'eagle'
+    console.log(args[3]); // prints 'hawk'
+};
+raptors('eagle', 'secretary bird', 'osprey', 'hawk', 'falcon');
