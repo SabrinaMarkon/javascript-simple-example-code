ab3139ed430fe68da3a7cfbcb34c8929196f9309
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-15 23:47:08 -0700

    Prototype simple examples

--- prototyping.js
+++ prototyping.js
@@ -9,3 +9,16 @@ prototype chain: object <- object's prototype <- object's object's prototype - i
 
 Prototype is automatically assigned, but we can DEFINE an object's prototype!
 */
+
+// Example:
+let obj = {} // it is 'undefined' in the console if we type obj
+
+// but methods still work on it? It has no methods itself so where does toString and other methods come from? Ans: obj's PROTOTYPE OBJECT.
+obj.toString(); // returns "[object Object]" <- where Object, the default object, is the prototype object for obj.
+
+obj.toString = function() { console.log("Object"); } // now it executes its own toString instead of the one above it in the chain.
+
+// everything in JS EXCEPT primitives is an object:
+let arr = [1, 2, 3];
+arr.indexOf[2]; // 1, because indexOf is in the Array prototype.
+
