6a83f09aa9b0d27f7c47bd77ce52dd57e9c06385
Sabrina Markon <phpsitescripts@outlook.com>
2018-02-15 23:32:18 -0700

    Notes on JS prototyping

---
+++ prototyping.js
@@ -0,0 +1,11 @@
+/* Prototyping */
+
+/* 
+Almost (not the default object or null objects) every object in JS has an internal property that is a reference to another object. It is linked to another object. This linked object is called the prototype.
+
+Objects inherit properties and methods from its prototype ancestry.
+
+prototype chain: object <- object's prototype <- object's object's prototype - inherits properties and methods down the chain.
+
+Prototype is automatically assigned, but we can DEFINE an object's prototype!
+*/
