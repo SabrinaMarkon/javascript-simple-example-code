873e7490fd9c89ab07b44b3f089e2b0108fe1c56
Sabrina Markon <phpsitescripts@outlook.com>
2018-03-25 00:02:41 -0600

    Notes for async flow example in promises 2

--- DevSpark-Promises-Tutorial/promises-2-multistepasyncflows.js
+++ DevSpark-Promises-Tutorial/promises-2-multistepasyncflows.js
@@ -40,8 +40,32 @@ function http(url, method) {
     });
 }
 
-/* call: */
-http('https://api.github.com/users?since=1364725', 'GET')
+/* CALL: */
+console.log('fetching users');
+http('https://api.github.com/users?since=1364725', 'GET') // get users (with user ID 1364725 first on list ie. index 0)
     .then(function(users) {
-        console.log(users);
+        console.log('fetching repos');
+        var user = users[0]; // get the first user object in the array of users returned from github.
+        return http(user.repos_url, 'GET'); /* get the user's repos_url property value. We can also add 'then' and 'catch' to individual returns like say:
+        return http('user.repos_url', 'GET').then().catch();
+        */
+        /* each new call to http returns a NEW PROMISE. Since we return the new promise in our 'then' function, we don't need any NESTING and can keep everything flat */
+    })
+    .catch() // say we want to have a catch function just for the two above only!!!!
+    .then(function(repos) {
+        console.log('fetching languages'); // show the repos fetched in the previous then function of the promise.
+        var repo= repos[0]; // get the first repo.
+        return http(repo.languages_url, 'GET');
+    })
+    .then(function(languages) {
+        console.log(languages);
+    })
+    .catch(function(err) {
+        console.log('Caught error:', err);
     });
+
+    /* RECAP */
+    /*
+    When we return a new promise from within our then, it becomes part of our promise chain. Our next then has access to the return value of the promise. It can use the SAME catch, or have its OWN. But we've removed the need for nested pyramid code and can keep everything flat. 
+    In this case, we needed to wait for our first promise to resolve before making our second call, because we grabbed specific data to use in the second call. 
+    */
